e7331d623e2d99e74305134a642bd022f93573be
TharinduSampath <gunasekeratharindu@gmail.com>
2023-03-21 12:59:08 +0530

    Database connection, Logging and further configuration

--- codebase/server/src/index.js
+++ codebase/server/src/index.js
@@ -1,1 +1,32 @@
-console.log("Hello world!");
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+import mongoose from "mongoose";
+import initializeLogger from "./utils/logger.js";
+dotenv.config();
+
+const log = initializeLogger(import.meta.url.split("/").pop() || "");
+
+log.info("Starting server...");
+const { PORT, MONGODB_URI } = process.env;
+
+const app = express();
+
+// Configuration
+log.info("Configuring middleware...");
+app.use(helmet());
+app.use(cors());
+
+// Routes
+log.info("Configuring routes...");
+
+// Database Connection
+log.info("Connecting to MongoDB Atlas...");
+await mongoose.connect(MONGODB_URI);
+log.info("Established connection");
+
+app.listen(
+    PORT || 3000,
+    () => { log.info(`Started listening to port ${PORT}`) }
+);
