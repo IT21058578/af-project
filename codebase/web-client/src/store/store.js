f622f2f24c464eb0f56ab1914a83529d13a0f4c0
TharinduSampath <gunasekeratharindu@gmail.com>
2023-05-17 00:09:27 +0530

    Added booking api slice

--- codebase/web-client/src/store/store.js
+++ codebase/web-client/src/store/store.js
@@ -1,7 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-// Or from '@reduxjs/toolkit/query/react'
-import { setupListeners } from "@reduxjs/toolkit/query";
-import { pokemonApi } from "./services/pokemon";
 import { baseApi } from "./api/base-api-slice";
 import { authReducer } from "./slices/auth-slice";
 
