419553eb2d15f842feae616fbb3cc9ac4038e42a
TharinduSampath <gunasekeratharindu@gmail.com>
2023-05-17 21:29:28 +0530

    Fixed rtk-query import issue

--- codebase/web-client/src/App.jsx
+++ codebase/web-client/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer";
-import Auth from "./pages/Auth/auth";
+import Auth from "./pages/Auth/Auth";
 import Blogs from "./pages/Blog/Blogs";
 import BlogDetail from "./pages/Blog/BlogDetails";
 import NewBlog from "./pages/NewBlog";
@@ -12,43 +12,35 @@ import Author from "./pages/Author";
 import UserOrder from "./pages/Tours/BookingHistory";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
 
-function App() {
+const App = () => {
 	return (
-		<Provider store={store}>
-			<BrowserRouter>
-				{/* <NavBar /> */}
-				<div className="App">
-					<Routes>
-						{/* 
+		<BrowserRouter>
+			{/* <NavBar /> */}
+			<div className="App">
+				<Routes>
+					{/* 
           <Route path="/posts" exact component={Home} />
           <Route path="/posts/search" exact component={Home} />
           <Route path="/posts/:id" exact component={PostDetails} />
           <Route path={['/creators/:name', '/tags/:name']} component={CreatorOrTag} /> */}
-						{/* <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/blogs" />)} /> */}
-						<Route path="/" exact component={() => <Redirect to="/blog" />} />
-						<Route exact path="/auth" element={<Auth />} />
-						<Route exact path="/blog" element={<Blogs />} />
-						<Route exact path="/new" element={<NewBlog />} />
-						<Route exact path="/tour" element={<TourHome />} />
-						<Route exact path="/details" element={<TourDetails />} />
-						<Route
-							exact
-							path="/tour/details/:title"
-							element={<TourDetails />}
-						/>
-						<Route exact path="/admin" element={<AdminHome />} />
-						<Route exact path="/blogdetail" element={<BlogDetail />} />
-						<Route exact path="/author" element={<Author />} />
-						<Route exact path="/userbooking" element={<UserOrder />} />
-					</Routes>
-				</div>
-				{/* <Footer /> */}
-			</BrowserRouter>
-		</Provider>
+					{/* <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/blogs" />)} /> */}
+					<Route path="/" exact component={() => <Redirect to="/blog" />} />
+					<Route exact path="/auth" element={<Auth />} />
+					<Route exact path="/blog" element={<Blogs />} />
+					<Route exact path="/new" element={<NewBlog />} />
+					<Route exact path="/tour" element={<TourHome />} />
+					<Route exact path="/details" element={<TourDetails />} />
+					<Route exact path="/tour/details/:title" element={<TourDetails />} />
+					<Route exact path="/admin" element={<AdminHome />} />
+					<Route exact path="/blogdetail" element={<BlogDetail />} />
+					<Route exact path="/author" element={<Author />} />
+					<Route exact path="/userbooking" element={<UserOrder />} />
+				</Routes>
+			</div>
+			{/* <Footer /> */}
+		</BrowserRouter>
 	);
-}
+};
 
 export default App;
