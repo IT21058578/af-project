e21e2533031fcedc39063af92a7b37502b3e1017
IT21058578 <it21058578@my.sliit.lk>
2023-05-19 18:55:05 +0530

    Added redux persist support. Auth is now persisted

--- codebase/web-client/src/main.jsx
+++ codebase/web-client/src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { persistor, store } from "./store/store";
+import { PersistGate } from "redux-persist/lib/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
--- codebase/web-client/src/store/store.js
+++ codebase/web-client/src/store/store.js
@@ -2,13 +2,40 @@ import { configureStore } from "@reduxjs/toolkit";
 import { baseApi } from "./api/base-api-slice";
 import { authReducer } from "./slices/auth-slice";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  blacklist: [baseApi.reducerPath],
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
-	reducer: {
-		auth: authReducer,
-		[baseApi.reducerPath]: baseApi.reducer,
-	},
-	middleware: (getDefaults) => getDefaults().concat(baseApi.middleware),
+  reducer: {
+    auth: persistedReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
+  },
+  middleware: (getDefaults) =>
+    getDefaults({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
+
+export const persistor = persistStore(store);
 
 setupListeners(store.dispatch);
